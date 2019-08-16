850899f0d93fada285a51c5f7fc3e2450e7a6ce6
ITyunqing <1186355422@qq.com>
2019-08-16 13:42:13 +0800

    seat init

---
+++ src/main/java/com/java4all/SeatApplication.java
@@ -0,0 +1,13 @@
+package com.java4all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatApplication.class, args);
+	}
+
+}
