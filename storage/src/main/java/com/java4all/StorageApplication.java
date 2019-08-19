da96d2938374ec76fc5827e63df5038c0600aab9
ITyunqing <1186355422@qq.com>
2019-08-19 14:49:58 +0800

    add storage

---
+++ storage/src/main/java/com/java4all/StorageApplication.java
@@ -0,0 +1,13 @@
+package com.java4all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageApplication.class, args);
+	}
+
+}
