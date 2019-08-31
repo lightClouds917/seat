f45cc90f4c367c16c25239a2309d216e5f36695f
ITyunqing <1186355422@qq.com>
2019-08-31 22:29:46 +0800

    add seata to storate

--- storage/src/main/java/com/java4all/storage/StorageApplication.java
+++ storage/src/main/java/com/java4all/storage/StorageApplication.java
@@ -1,8 +1,12 @@
 package com.java4all.storage;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import javax.sql.DataSource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author IT云清
@@ -13,6 +17,13 @@ public class StorageApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StorageApplication.class, args);
+	}
+
+	@Bean
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DataSource dataSource(){
+		DruidDataSource dataSource = new DruidDataSource();
+		return dataSource;
 	}
 
 }
