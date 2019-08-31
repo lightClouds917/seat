0c655b76620d607743c4ba58f0a7988bd56c94cc
ITyunqing <1186355422@qq.com>
2019-08-31 21:55:00 +0800

    add seata to account

--- account/src/main/java/com/java4all/account/AccountApplication.java
+++ account/src/main/java/com/java4all/account/AccountApplication.java
@@ -1,9 +1,13 @@
 package com.java4all.account;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import javax.sql.DataSource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author IT云清
@@ -16,5 +20,14 @@ public class AccountApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AccountApplication.class, args);
 	}
+
+	@Bean
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DataSource dataSource(){
+		DruidDataSource dataSource = new DruidDataSource();
+		return dataSource;
+	}
+
+	//TODO 需不需要dataSourceProxy？
 
 }
