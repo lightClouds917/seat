72d2ac0e7d00ada6825c7f7a9b1c48328104ebf5
ITyunqing <1186355422@qq.com>
2019-09-02 09:10:54 +0800

    add dataSource to order

--- order/src/main/java/com/java4all/order/DataSourceConfiguration.java
+++ order/src/main/java/com/java4all/order/DataSourceConfiguration.java
@@ -1,10 +1,14 @@
 package com.java4all.order;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import io.seata.rm.datasource.DataSourceProxy;
 import javax.sql.DataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 /**
  * @author wangzhongxiang
@@ -12,11 +16,25 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DataSourceConfiguration {
 
-    @Bean
+    @Bean("dataSource")
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource(){
-        DruidDataSource dataSource = new DruidDataSource();
-        return dataSource;
+        return new DruidDataSource();
+    }
+
+
+    @Primary
+    @Bean("dataSourceProxy")
+    public DataSourceProxy dataSourceProxy(DataSource dataSource){
+        return new DataSourceProxy(dataSource);
+    }
+
+
+    @Bean("sqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactory(DataSourceProxy dataSourceProxy)throws Exception{
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSourceProxy);
+        return sqlSessionFactoryBean.getObject();
     }
 
 }
