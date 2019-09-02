126d75c9c898c38a221f239db3fe4b0601179360
ITyunqing <1186355422@qq.com>
2019-09-02 09:13:10 +0800

    add dataSource to storage

--- storage/src/main/java/com/java4all/storage/StorageApplication.java
+++ storage/src/main/java/com/java4all/storage/StorageApplication.java
@@ -1,12 +1,16 @@
 package com.java4all.storage;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import io.seata.rm.datasource.DataSourceProxy;
 import javax.sql.DataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 /**
  * @author IT云清
@@ -24,6 +28,22 @@ public class StorageApplication {
 	public DataSource dataSource(){
 		DruidDataSource dataSource = new DruidDataSource();
 		return dataSource;
+	}
+
+
+
+	@Primary
+	@Bean("dataSourceProxy")
+	public DataSourceProxy dataSourceProxy(DataSource dataSource){
+		return new DataSourceProxy(dataSource);
+	}
+
+
+	@Bean("sqlSessionFactory")
+	public SqlSessionFactory sqlSessionFactory(DataSourceProxy dataSourceProxy)throws Exception{
+		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		sqlSessionFactoryBean.setDataSource(dataSourceProxy);
+		return sqlSessionFactoryBean.getObject();
 	}
 
 }
